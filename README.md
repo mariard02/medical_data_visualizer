57826035c955f479019a1d576f81fa8ee9de3d61
María <maria.rd02@gmail.com>
2024-12-01 16:15:47 +0100

    Initial commit. Did the initial filtering

---
+++ README.md
@@ -0,0 +1,3 @@
+# Medical Data Visualizer
+
+Medical Data Visualizer project for the Data Analysis with Python certification from Freecodecamp. Instructions for building the project can be found at https://www.freecodecamp.org/learn/data-analysis-with-python/data-analysis-with-python-projects/medical-data-visualizer
---
+++ medical_data_visualizer.py
@@ -0,0 +1,63 @@
+import pandas as pd
+#import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 1
+df = pd.read_csv("medical_examination.csv")
+
+# 2
+BMI = df["weight"] / (df["height"] / 100)**2
+df['overweight'] = (BMI > 25).astype(int)
+
+
+# 3
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
+
+# 4
+def draw_cat_plot():
+    # 5
+    df_cat = None
+
+
+    # 6
+    df_cat = None
+    
+
+    # 7
+
+
+
+    # 8
+    fig = None
+
+
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+
+# 10
+def draw_heat_map():
+    # 11
+    df_heat = None
+
+    # 12
+    corr = None
+
+    # 13
+    mask = None
+
+
+
+    # 14
+    fig, ax = None
+
+    # 15
+
+
+
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
