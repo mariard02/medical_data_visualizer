71931ce9706cc516784a7f045b6caa98b02b0ed4
María <maria.rd02@gmail.com>
2024-12-01 16:52:29 +0100

    Created the category plot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,5 +1,5 @@
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,20 +17,39 @@ df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # 1. Convertir el DataFrame a formato largo
+    df_cat = pd.melt(
+        df, 
+        id_vars=["cardio"], 
+        value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"],
+        var_name="variable", 
+        value_name="value"    
+    )
 
+    #df_cat = df_cat.groupby(["cardio", "variable", "value"]).size().reset_index(name="count")
+    df_cat = df_cat.groupby("cardio").value_counts()
+    df_cat = df_cat.reset_index()
 
-    # 6
-    df_cat = None
+    print(df_cat.head())
     
+    cat_plot = sns.catplot(
+        x="variable",  
+        y="count",    
+        hue="value",   
+        col="cardio",  
+        data=df_cat,   
+        kind="bar",    
+        height=5,      
+        aspect=1       
+    )
 
-    # 7
-
-
+    cat_plot.set_axis_labels("Variable", "Conteo")
+    cat_plot.set_titles("Cardio = {col_name}")
+    #cat_plot.despine(left=True)
+    plt.show()
 
     # 8
-    fig = None
+    fig = cat_plot
 
 
     # 9
